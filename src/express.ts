29a7b0d2902ecc62ccdc37f8e7e05ada8bfc5456
Robin Panta <hacktivistic@gmail.com>
2022-02-01 05:32:20 +0530

    feat: add error handler

--- src/express.ts
+++ src/express.ts
@@ -1,12 +1,19 @@
 import * as express from 'express'
 import { Express } from 'express'
+import * as createError from 'http-errors'
 import { RouterDispatcher } from '@haluka/routing'
 import { IRouterDispatcher } from '@haluka/routing/build/Routing/RoutingEssentials'
 
-export default class ExpressDispatcher extends RouterDispatcher<Express> implements IRouterDispatcher {
+export default abstract class ExpressDispatcher extends RouterDispatcher<Express> implements IRouterDispatcher {
 
     create (): Express {
         let app =  express()
+
+        if (this.errorHandler)
+            app.use(this.errorHandler)
+
+        globalThis.httpError = createError
+        
         return app
     }
 
@@ -21,25 +28,37 @@ export default class ExpressDispatcher extends RouterDispatcher<Express> impleme
                 express[method.toLowerCase()](route.uri, ...middlewares, this.wrap(route.compiledAction)) 
             });
 		}
+
+        // For Handling 404
+        express.use(function (_req: any, _res: any, next: (arg0: any) => void) {
+            next(createError(404))
+        })
+
 		return express
     }
 
-    onRequest (req, res) {
-        //
-    }
+    abstract onRequest (req: any, res: any): void
 
-    onResponse (req, res, output) {
-        //
-    }
+    abstract onResponse (req: any, res: any, output: any): void
+
+    abstract errorHandler (err: any, req: any, res: any, next: any): void
 
     protected wrap (action: Function) {
-        return (req: any, res: any) => {
-            this.onRequest(req, res)
-            let ret = action({req, res, Request: req, Response: res})
-            // if (!res.headersSent) {
-            //     res.end(ret)
+        return async (req: any, res: any) => {
+            // try {
+                // To provide custom request handlers
+                this.onRequest(req, res)
+                // Execute the action
+                let ret = await action({req, res, Request: req, Response: res})
+
+                // TODO: fallback handler (when no response is sent)
+
+                // To provide custom response handlers
+                this.onResponse(req, res, ret)
+            // } catch (error) {
+                
             // }
-            this.onResponse(req, res, ret)
+            
         }
     }
 
