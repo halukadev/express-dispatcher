4cc34ad62afc919a616d61ca46d17fa7186d773d
Robin Panta <hacktivistic@gmail.com>
2022-02-01 07:15:03 +0530

    build

--- build/express.d.ts
+++ build/express.d.ts
@@ -1,10 +1,11 @@
 import { Express } from 'express';
 import { RouterDispatcher } from '@haluka/routing';
 import { IRouterDispatcher } from '@haluka/routing/build/Routing/RoutingEssentials';
-export default class ExpressDispatcher extends RouterDispatcher<Express> implements IRouterDispatcher {
+export default abstract class ExpressDispatcher extends RouterDispatcher<Express> implements IRouterDispatcher {
     create(): Express;
     dispatch(express: Express): Express;
-    onRequest(req: any, res: any): void;
-    onResponse(req: any, res: any, output: any): void;
-    protected wrap(action: Function): (req: any, res: any) => void;
+    abstract onRequest(req: any, res: any): void;
+    abstract onResponse(req: any, res: any, output: any): void;
+    abstract errorHandler(err: any, req: any, res: any, next: any): any;
+    protected wrap(action: Function): (req: any, res: any, next: CallableFunction) => Promise<void>;
 }
--- build/express.js
+++ build/express.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const createError = require("http-errors");
 const routing_1 = require("@haluka/routing");
+globalThis.httpError = createError;
 class ExpressDispatcher extends routing_1.RouterDispatcher {
     create() {
         let app = express();
@@ -17,22 +19,32 @@ class ExpressDispatcher extends routing_1.RouterDispatcher {
                 express[method.toLowerCase()](route.uri, ...middlewares, this.wrap(route.compiledAction));
             });
         }
+        // For Handling 404
+        express.use(function (_req, _res, next) {
+            next(createError(404));
+        });
+        express.use((err, req, res, next) => {
+            let resp = this.errorHandler(err, req, res, next);
+            if (!resp)
+                return next(err);
+            next(resp);
+        });
         return express;
     }
-    onRequest(req, res) {
-        //
-    }
-    onResponse(req, res, output) {
-        //
-    }
     wrap(action) {
-        return (req, res) => {
-            this.onRequest(req, res);
-            let ret = action({ req, res, Request: req, Response: res });
-            // if (!res.headersSent) {
-            //     res.end(ret)
-            // }
-            this.onResponse(req, res, ret);
+        return async (req, res, next) => {
+            try {
+                // To provide custom request handlers
+                this.onRequest(req, res);
+                // Execute the action
+                let ret = await action({ req, res, Request: req, Response: res });
+                // TODO: fallback handler (when no response is sent)
+                // To provide custom response handlers
+                this.onResponse(req, res, ret);
+            }
+            catch (error) {
+                next(createError(error));
+            }
         };
     }
 }
